70dda8718ff39a641f82e3d199fd7c89df850a04
manuelvargastapia <manuelvargastapia@gmail.com>
2020-11-12 17:06:37 -0300

    new feture: search by ids

---
+++ examples/search_by_ids.js
@@ -0,0 +1,26 @@
+const libgen = require('../index.js');
+
+(async () => {
+  const options = {
+    mirror: 'http://libgen.is',
+    ids: '430,86' // could also be ["430", "86"]
+  }
+
+  try {
+    const data = await libgen.search(options)
+    let n = data.length;
+    console.log('top ' + n + ' results for "' +
+                options.query + '"' + ' seaching by ids ' + options.ids.toString());
+    while (n--){
+      console.log('***********');
+      console.log('Title: ' + data[n].title);
+      console.log('Author: ' + data[n].author);
+      console.log('Download: ' +
+                  'http://gen.lib.rus.ec/book/index.php?md5=' +
+                  data[n].md5.toLowerCase());
+    }
+    return true
+  } catch (err) {
+    return console.error(err)
+  }
+})();
